1156f0ff0d8f1924370c07c96f6893db7df027ed
mattes <matthias.kadenbach@gmail.com>
2013-06-27 11:44:44 +0200

    updated init.pp file

--- puppet/manifests/init.pp
+++ puppet/manifests/init.pp
@@ -1,10 +1,24 @@
-exec { "apt_update":
-  command => "apt-get update",
-  path    => "/usr/bin"
+# use paths for Exec statements
+Exec { path => [ "/bin/", "/sbin/" , "/usr/bin/", "/usr/sbin/" ] }
+
+# virtual resource
+@exec { 'sudo apt-get update':
+   tag => update
 }
 
-  class{'git::install':}
-  class{'apache2::install':}
-  class{'php5::install':}  
-  class{'mysql::install':}
-  class{'wordpress::install':}
+# realize resource. filter by "update" tag
+# and relate it to all Package resources
+Exec <| tag == update |> -> Package <| |>
+
+
+node default {
+
+  class { 'git::install': }
+  class { 'apache2::install': }
+  class { 'php5::install': }  
+  class { 'mysql::install': }
+  
+  # class{'wordpress::install':}
+  # use wp-cli instead?
+  # https://github.com/wp-cli/wp-cli
+}
