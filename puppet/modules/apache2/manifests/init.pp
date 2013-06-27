0572d95cc2afee219e4de2f58562beacd2a4bb46
mattes <matthias.kadenbach@gmail.com>
2013-06-27 10:42:11 +0200

    rewrite apache2 manifest

--- puppet/modules/apache2/manifests/init.pp
+++ puppet/modules/apache2/manifests/init.pp
@@ -15,4 +15,28 @@ class apache2::install{
       notify  => Service["apache2"],
   }
 
+  # disable default site after fresh apache installation
+  exec { "a2dissite 000-default":
+     command => "a2dissite 000-default",
+     onlyif => "test -f /etc/apache2/sites-enabled/000-default",
+     require => Package["apache2"],
+     notify  => Service["apache2"],
+  }
+
+  # enable mod vhost_alias
+  exec { "a2enmod vhost_alias":
+     command => "a2enmod vhost_alias",
+     creates => '/etc/apache2/mods-enabled/vhost_alias.load',
+     require => Package["apache2"],
+     notify  => Service["apache2"],
+  }
+
+  # enable mod rewrite
+  exec { "a2enmod rewrite":
+      command => "a2enmod rewrite",
+      creates => '/etc/apache2/mods-enabled/rewrite.load',
+      require => Package["apache2"],
+      notify  => Service["apache2"],
+  }
+
 }
