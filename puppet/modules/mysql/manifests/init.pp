79d32051b4b8d3a52e8bffd810a430779da187ba
mattes <matthias.kadenbach@gmail.com>
2013-06-30 19:14:52 +0200

    allow remote access to mysql server

--- puppet/modules/mysql/manifests/init.pp
+++ puppet/modules/mysql/manifests/init.pp
@@ -1,14 +1,30 @@
 class mysql::install {
   $password = "vagrant"
+
+
   package { "mysql-client": ensure => installed }
   package { "mysql-server": ensure => installed }
 
-  exec { "Set MySQL server root password":
+ # service { "mysql":
+ #    enable => true,
+ #    ensure => running,
+ #    require => Package["mysql-server"],
+ #  }
+
+  exec { "Set MySQL server root password and allow remote access for root":
     subscribe => [ Package["mysql-server"], Package["mysql-client"] ],
     refreshonly => true,
     unless => "mysqladmin -uroot -p$password status",
     path => "/bin:/usr/bin",
-    command => "mysqladmin -uroot password $password",
+    command => "mysqladmin -uroot password $password; mysql --user='root' --password='vagrant' -e \"CREATE USER 'root' IDENTIFIED BY 'vagrant'; GRANT ALL ON *.* TO 'root'@'%'; FLUSH PRIVILEGES;\"",
+  }
+
+  # use our httpd.conf file
+  file { "/etc/mysql/my.cnf":
+      ensure => link,
+      source => "puppet:///modules/mysql/mysql-my.cnf",
+      require => Package['mysql-server'],
+      # notify  => Service["mysql"],
   }
 
   # exec { "Grant MySQL server remote access":
